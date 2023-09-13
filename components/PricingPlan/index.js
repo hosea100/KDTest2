8cd8c65e21d71ab2de3f50d14bdb1c507612f2d9
Hosea Yoarana <hoseayoarana@gmail.com>
2023-09-14 02:30:54 +0700

    pricing clear

--- components/PricingPlan/index.js
+++ components/PricingPlan/index.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from 'react'
+import { Icon } from "@iconify/react";
 
 function PricingPlan() {
   const [selectedItem, setselectedItem] = useState("monthly");
@@ -8,7 +9,7 @@ function PricingPlan() {
   };
   return (
     <div
-      className='bg-dracula-purple bg-cover bg-center min-h-screen flex flex-col justify-center items-center'
+      className='bg-dracula-purple bg-cover bg-left min-h-screen flex flex-col justify-center items-center sm:bg-center'
       id='pricing'
       style={{ backgroundImage: 'url("/Img/blob-haikei3.svg")' }}
     >
@@ -18,202 +19,200 @@ function PricingPlan() {
             <p className="text-dracula-orange font-bold">Make Your Business Thrive with Us</p>
             <h2 className="text-3xl font-bold">Yes <span className='text-dracula-green'>PRICE</span>, so <span className='text-dracula-green'>CHEAP</span> la</h2>
           </div>
-          {/* <div className="text-center py-4">
-            <div className="flex justify-center">
-              <div className="w-64 flex">
-                <div
-                  className={`w-1/2 p-0 cursor-pointer ${selectedItem === "trial"
-                    ? "bg-black text-white"
-                    : "bg-white"
-                    }`}
-                  onClick={() => onClickItem("trial")}
-                >
-                  <a className="block py-3 text-center">Free trial</a>
+          <div className="flex flex-col justify-center py-4 sm:flex-row">
+            <div className="w-80 m-5 text-center px-4 py-5 bg-white shadow-md rounded-lg">
+              <h4 className="text-dracula-selection font-bold">Basic</h4>
+              <h1 className="text-5xl font-semibold">$15</h1>
+              <h5 className="mb-4">Monthly Subscription</h5>
+              <button className="bg-dracula-purple hover:bg-dracula-cyan text-white font-bold py-2 px-4 border-b-4 border-dracula-comment hover:border-dracula-comment rounded">Get Basic Plan</button>
+              <p className="my-4">
+                Description lorem ipsum same with platinum (?) version with
+                30 days
+              </p>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">Mencatat barang masuk</a>
                 </div>
-                <div
-                  className={`w-1/2 p-0 cursor-pointer ${selectedItem === "monthly"
-                    ? "bg-black text-white"
-                    : "bg-white"
-                    }`}
-                  onClick={() => onClickItem("monthly")}
-                >
-                  <a className="block py-3 text-center">Monthly</a>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">Mencatat barang keluar</a>
+                </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">Mencatat hasil keuntungan</a>
+                </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-gray-500">
+                  <Icon
+                    icon="material-symbols:cancel-outline"
+
+                  />
+                  <a className="ml-2">Analisa penjualan dengan CHART</a>
+                </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-gray-500">
+                  <Icon
+                    icon="material-symbols:cancel-outline"
+
+                  />
+                  <a className="ml-2 ">Support 7x24 Jam</a>
+                </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-gray-500">
+                  <Icon
+                    icon="material-symbols:cancel-outline"
+                  />
+                  <a className="ml-2">Export data ke Excel</a>
+                </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-gray-500">
+                  <Icon
+                    icon="material-symbols:cancel-outline"
+                  />
+                  <a className="ml-2">AI Prediksi penghasilan</a>
                 </div>
               </div>
             </div>
-          </div> */}
-          {selectedItem === "trial" ? (
-            <div className="flex justify-center py-4">
-              <div className="w-64 m-3 text-center px-4 py-5 bg-white shadow-md rounded-lg">
-                <h4 className="text-orange-500 font-bold">Basic</h4>
-                <h1 className="text-5xl font-semibold">$0</h1>
-                <button className="btn-plan">Get 30-days Trial</button>
-                <p className="my-4">
-                  Description lorem ipsum same with platinum (?) version with
-                  30 days
-                </p>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Interactive menu</a>
-                  </div>
+            <div className="w-80 m-5 text-center px-4 py-5 bg-white shadow-md rounded-lg scale-100 sm:scale-110">
+              <h4 className="text-dracula-orange font-bold">Business</h4>
+              <h1 className="text-5xl font-semibold">$50</h1>
+              <h5 className="mb-4">Monthly Subscription</h5>
+              <button className="bg-dracula-purple hover:bg-dracula-cyan text-white font-bold py-2 px-4 border-b-4 border-dracula-comment hover:border-dracula-comment rounded">Get Business Plan</button>
+              <p className="my-4">
+                Description lorem ipsum same with platinum (?) version with
+                30 days
+              </p>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">Mencatat barang masuk</a>
                 </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Table management</a>
-                  </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">Mencatat barang keluar</a>
                 </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Dine in management</a>
-                  </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">Mencatat hasil keuntungan</a>
                 </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Take away management</a>
-                  </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">Analisa penjualan dengan CHART</a>
                 </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Restaurant report</a>
-                  </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">Support 7x24 Jam</a>
+                </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-gray-500">
+                  <Icon
+                    icon="material-symbols:cancel-outline"
+                  />
+                  <a className="ml-2">Export data ke Excel</a>
+                </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-gray-500">
+                  <Icon
+                    icon="material-symbols:cancel-outline"
+                  />
+                  <a className="ml-2">AI Prediksi penghasilan</a>
                 </div>
               </div>
             </div>
-          ) : (
-            <div className="flex justify-center py-4">
-              <div className="w-64 m-3 text-center px-4 py-5 bg-white shadow-md rounded-lg">
-                <h4 className="text-dracula-selection font-bold">Basic</h4>
-                <h1 className="text-5xl font-semibold">$15</h1>
-                <h5 className="mb-4">Monthly Subscription</h5>
-                <button className="btn-plan">Get Basic Plan</button>
-                <p className="my-4">
-                  Description lorem ipsum same with platinum (?) version with
-                  30 days
-                </p>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Mencatat barang masuk</a>
-                  </div>
-                </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Mencatat barang keluar</a>
-                  </div>
-                </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-gray-700">Mencatat hasil keuntungan</a>
-                  </div>
-                </div>
-                {/* <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-gray-700">Take away management</a>
-                  </div>
-                </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-gray-700">Restaurant report</a>
-                  </div>
-                </div> */}
-              </div>
-              <div className="w-64 m-3 text-center px-4 py-5 bg-white shadow-md rounded-lg">
-                <h4 className="text-dracula-orange font-bold">Business</h4>
-                <h1 className="text-5xl font-semibold">$50</h1>
-                <h5 className="mb-4">Monthly Subscription</h5>
-                <button className="btn-plan">Get Business Plan</button>
-                <p className="my-4">
-                  Description lorem ipsum same with platinum (?) version with
-                  30 days
-                </p>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Mencatat barang masuk</a>
-                  </div>
-                </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Mencatat barang keluar</a>
-                  </div>
-                </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Mencatat hasil keuntungan</a>
-                  </div>
-                </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-gray-700">Dapat menganalisa hasil penjualan dengan CHART</a>
-                  </div>
-                </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-gray-700">Support 7x24 Jam</a>
-                  </div>
+            <div className="w-80 m-5 text-center px-4 py-5 bg-white shadow-md rounded-lg">
+              <h4 className="text-dracula-selection font-bold">Entrepreneur</h4>
+              <h1 className="text-5xl font-semibold">$100</h1>
+              <h5 className="mb-4">Monthly Subscription</h5>
+              <button className="bg-dracula-purple hover:bg-dracula-cyan text-white font-bold py-2 px-4 border-b-4 border-dracula-comment hover:border-dracula-comment rounded">Get Entrepreneur Plan</button>
+              <p className="my-4">
+                Description lorem ipsum same with platinum (?) version with
+                30 days
+              </p>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">Mencatat barang masuk dan keluar</a>
                 </div>
               </div>
-              <div className="w-64 m-3 text-center px-4 py-5 bg-white shadow-md rounded-lg">
-                <h4 className="text-dracula-selection font-bold">Entrepreneur</h4>
-                <h1 className="text-5xl font-semibold">$100</h1>
-                <h5 className="mb-4">Monthly Subscription</h5>
-                <button className="btn-plan">Get Entrepreneur Plan</button>
-                <p className="my-4">
-                  Description lorem ipsum same with platinum (?) version with
-                  30 days
-                </p>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Mencatat barang masuk dan keluar</a>
-                  </div>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">Mencatat Keuntungan</a>
                 </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Mencatat Keuntungan</a>
-                  </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">Analisa penjualan dengan CHART</a>
                 </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Dapat menganalisa hasil penjualan dengan CHART</a>
-                  </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">Support 7x24 Jam</a>
                 </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Support 7x24 Jam</a>
-                  </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">Export data ke Excel</a>
                 </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">Export data ke Excel</a>
-                  </div>
-                </div>
-                <div className="my-3">
-                  <div className="flex items-center">
-                    <img src="/assets/pricing/check.png" alt="Check" />
-                    <a className="ml-2 text-black">AI Prediksi penghasilan</a>
-                  </div>
+              </div>
+              <div className="my-3">
+                <div className="flex items-center text-green-500">
+                  <Icon
+                    icon="zondicons:checkmark-outline"
+                  />
+                  <a className="ml-2">AI Prediksi penghasilan</a>
                 </div>
               </div>
             </div>
-          )}
+          </div>
         </div>
       </div>
     </div>
